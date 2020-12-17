e38ba651ff966cc131820e55d36f327b905f3d8b
lou <louamaya@me.com>
2020-12-17 06:25:31 -0800

    initialize app.py and add homepage get route

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import Pet
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'iamlou'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension
+
+app.config['SQLALCHEMY_DATABSE_URI'] = "postgresql://pet_adoption"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+
+@app.route("/")
+def home():
+    """Manages home page"""
+    return render_template("index.html")
