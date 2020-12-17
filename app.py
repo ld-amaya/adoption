37da222fbfd3deb4b4a6c47d4387cba3b0202485
lou <louamaya@me.com>
2020-12-17 07:25:55 -0800

    add pets to be populated to index page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import Pet
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iamlou'
@@ -8,7 +8,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension
 
-app.config['SQLALCHEMY_DATABSE_URI'] = "postgresql://pet_adoption"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///pet_adoption"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
