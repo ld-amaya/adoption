5c9f586413230ec76afbc9e375601b4884a0280b
lou <louamaya@me.com>
2020-12-21 01:56:31 -0800

    add form fields from wtforms

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange
+
+
+class PetForms(FlaskForm):
+    """Define forms to be used in Pet Adoption"""
+
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()])
+
+    species = SelectField(
+        "Species",
+        choices=[("cat", "Cat"),
+                 ("dog", "Dog"),
+                 ("rabbit", "Rabbit")])
+
+    photo = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()])
+
+    age = IntegerField(
+        "Age",
+        validators=[Optional(),
+                    NumberRange(min=0, max=25)])
+
+    notes = TextAreaField("Comments / Notes",
+                          validators=[Optional()])
