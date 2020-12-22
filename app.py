51a471c2f42ae80a0fddb3372b869c07e492a187
lou <louamaya@me.com>
2020-12-22 03:09:04 -0800

    add more routes for editing, displaying cat, api requests

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, redirect, session, flash
+from flask import Flask, request, render_template, redirect, session, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
-from forms import PetForms
+from forms import PetForms, EditPetForm
 
 app = Flask(__name__)
 # app.debug = True
@@ -19,6 +19,7 @@ connect_db(app)
 db.create_all()
 
 
+# GET Requests
 @app.route("/")
 def home():
     """Manages home page"""
@@ -26,6 +27,35 @@ def home():
     return render_template("index.html", pets=pets)
 
 
+@app.route("/species/<specie>")
+def view_specie(specie):
+    """Handles Specific Specie"""
+    pets = Pet.query.filter_by(species=specie).all()
+    return render_template("/species.html", pets=pets)
+
+
+@app.route("/available")
+def view_available_pets():
+    """Displays Available Pets Only"""
+    pets = Pet.query.filter_by(available=True).all()
+    return render_template("index.html", pets=pets)
+
+
+@app.route("/not_available")
+def view_not_available_pets():
+    """Displays Not Available Pets Only"""
+    pets = Pet.query.filter_by(available=False).all()
+    return render_template("index.html", pets=pets)
+
+
+@app.route("/<int:id>")
+def view_pet(id):
+    """Display Pet Information"""
+    pet = Pet.query.get_or_404(id)
+    return render_template("/pet.html", pet=pet)
+
+
+######### CREATE REQUESTS ###########################
 @app.route("/add", methods=["GET", "POST"])
 def add_pet():
     """Handles the add pet form"""
@@ -46,3 +76,46 @@ def add_pet():
         return redirect("/")
     else:
         return render_template("/add_pet.html", form=form)
+
+
+######## UPDATE REQUESTS #####################################
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_pet(id):
+    """Display Pet Information"""
+    pet = Pet.query.get_or_404(id)
+    form = EditPetForm(obj=pet)
+    if form.validate_on_submit():
+        pet.photo = form.photo.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        flash(f"Pet {pet.name} Updated")
+        return redirect(f"/{pet.id}")
+    else:
+        return render_template("/pet_edit.html", pet=pet, form=form)
+
+
+######### Handles API GET Requests ###################################
+
+@app.route("/api/pets/<int:id>")
+def pet_api_id_based(id):
+    """Return basic information about a pet id"""
+    pet = Pet.query.get_or_404(id)
+    info = {"name": pet.name,
+            "species": pet.species,
+            "age": pet.age}
+    return jsonify(info)
+
+
+@app.route("/api/pets")
+def pet_api_all():
+    """Return basic information about the pet"""
+    pets = Pet.query.all()
+    data = []
+    for pet in pets:
+        info = {'name': pet.name,
+                'species': pet.species,
+                'age': pet.age}
+        data.append(info)
+    return jsonify(data)
